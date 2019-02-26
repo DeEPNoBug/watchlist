a8997ba725b4fc9e26cae46e31eea3364b8a535c
deepnobug <deepnobug@gmail.com>
2019-02-26 10:21:23 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,83 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+flask 配置
+http://flask-sqlalchemy.pocoo.org/2.1/config/
 
+flask-sqlalchemy配置
+http://flask-sqlalchemy.pocoo.org/2.1/config/
+"""
+import os
+import sys
+import click
 
 from flask import Flask, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是windows系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop')
+def initdb(drop):
+    """Initalize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database.")  # 输出提示信息
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data"""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = "Grey Li"
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'}
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
 
 
 @app.route('/')
